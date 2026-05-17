c77356b0a2dd61e28180ede0dcb01fdb2f7828b9
ToxOptimism <toxicoptimismben@gmail.com>
2026-05-17 12:42:11 +0800

    feat: add attack component superclass

---
+++ src/scripts/Entities/Player/Shared/Attacks/AttackComponent.cs
@@ -0,0 +1,124 @@
+using Godot;
+using System;
+
+public partial class AttackComponent : Node2D, IActionComponent
+{
+    [ExportCategory("Attack")]
+    [Export] public float attackCooldown = 1f;
+    [Export] public float attackDuration = 0.5f;
+    [Export] public int attackDamage = 1;
+
+    [ExportCategory("Locks")]
+    [Export] public bool lockMovementDuringAttack;
+    [Export] public bool lockDashDuringAttack;
+
+    private readonly Timer _cooldownTimer = new Timer();
+    
+    private bool _attackQueued;
+    private bool _justAttacked;
+    
+    private Player _player;
+    private PlayerInput _input;
+    private ActionOrchestrator _orchestrator;
+
+    public void Init(Player player)
+    {
+        _player = player;
+        _input = player.Input;
+        _orchestrator = player.Orchestrator;
+    }
+
+    public virtual void PrePhysicsUpdate(float dt)
+    {
+        _attackQueued = false;
+        _justAttacked = false;
+
+        if (!AttackTriggered())
+        {
+            return;
+        }
+
+        if (!CanAttack())
+        {
+            return;
+        }
+
+        _attackQueued = true;
+    }
+
+    public virtual void PhysicsUpdate(float dt)
+    {
+        if (_attackQueued)
+        {
+            _cooldownTimer.Start(attackCooldown);
+            ApplyLocks();
+            ExecuteAttack();
+            _justAttacked = true;
+        }
+    }
+
+    public virtual void PostPhysicsUpdate(float dt)
+    {   
+        if (!_justAttacked)
+        {
+            UpdateCooldown(dt);  
+        } 
+    }
+
+    public virtual bool AttackTriggered()
+    {
+        if (!_input.current.attack1Held)
+        {
+            return false;
+        }
+        
+        return true;
+    }
+
+    public virtual bool CanAttack()
+    {
+        return !_cooldownTimer.IsRunning && _orchestrator.CanAttack();
+    }
+
+    protected virtual void ExecuteAttack(){}
+
+    protected virtual void ApplyLocks()
+    {
+        _orchestrator.AddTag("AttackLocked");
+
+        if (lockMovementDuringAttack)
+        {
+            _orchestrator.AddTag("MovementLocked");
+        }
+
+        if (lockDashDuringAttack)
+        {
+            _orchestrator.AddTag("DashLocked");
+        }
+    }
+
+    protected virtual void RemoveLocks()
+    {
+        _orchestrator.RemoveTag("AttackLocked");
+
+        if (lockMovementDuringAttack)
+        {
+            _orchestrator.RemoveTag("MovementLocked");
+        }
+
+        if (lockDashDuringAttack)
+        {
+            _orchestrator.RemoveTag("DashLocked");
+        }
+    }
+
+    private void UpdateCooldown(float dt)
+    {
+        _cooldownTimer.Tick(dt);
+
+        if (_cooldownTimer.HasStopped)
+        {
+            RemoveLocks();
+        }
+    }
+}
