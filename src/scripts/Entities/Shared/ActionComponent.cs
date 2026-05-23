ceef99f8807415f139697cc65219186e9c277250
ToxOptimism <toxicoptimismben@gmail.com>
2026-05-23 23:28:17 +0800

    feat: add priority to action orchestrator

--- src/scripts/Entities/Shared/ActionComponent.cs
+++ src/scripts/Entities/Shared/ActionComponent.cs
@@ -2,6 +2,10 @@ using Godot;
 
 public interface IActionComponent
 {
+    int PrePhysicsPriority => 0;
+    int PhysicsPriority => 0;
+    int PostPhysicsPriority => 0;
+    
     void Init(Node owner);
 
     virtual void PrePhysicsUpdate(float dt) {}
--- src/scripts/Entities/Shared/ActionOrchestrator.cs
+++ src/scripts/Entities/Shared/ActionOrchestrator.cs
@@ -6,6 +6,9 @@ public partial class ActionOrchestrator : Node
 	private Node _owner;
 
 	private readonly List<IActionComponent> _components = new();
+	private readonly List<IActionComponent> _prePhysics = new();
+	private readonly List<IActionComponent> _physics = new();
+	private readonly List<IActionComponent> _postPhysics = new();
 
 	private readonly HashSet<string> _tags = new();
 
@@ -13,6 +16,7 @@ public partial class ActionOrchestrator : Node
 	{
 		_owner = owner;
 		RegisterChildren(this);
+		BuildSortedLists();
 	}
 
 	private void RegisterChildren(Node node) 
@@ -23,15 +27,56 @@ public partial class ActionOrchestrator : Node
 			{ 
 				component.Init(_owner); 
 				_components.Add(component); 
-			} 
-			RegisterChildren(child); 
+			}
+		}
+	}
+
+	private void BuildSortedLists()
+	{
+		_prePhysics.Clear();
+		_physics.Clear();
+		_postPhysics.Clear();
+
+		_prePhysics.AddRange(_components);
+		_physics.AddRange(_components);
+		_postPhysics.AddRange(_components);
+
+		_prePhysics.Sort((a, b) => a.PrePhysicsPriority.CompareTo(b.PrePhysicsPriority));
+		_physics.Sort((a, b) => a.PhysicsPriority.CompareTo(b.PhysicsPriority));
+		_postPhysics.Sort((a, b) => a.PostPhysicsPriority.CompareTo(b.PostPhysicsPriority));
+	}
+
+	public void RegisterComponent(IActionComponent component)
+	{
+		if (_components.Contains(component))
+			return;
+
+		component.Init(_owner);
+		_components.Add(component);
+
+		InsertSorted(_prePhysics, component, (a, b) => a.PrePhysicsPriority.CompareTo(b.PrePhysicsPriority));
+		InsertSorted(_physics, component, (a, b) => a.PhysicsPriority.CompareTo(b.PhysicsPriority));
+		InsertSorted(_postPhysics, component, (a, b) => a.PostPhysicsPriority.CompareTo(b.PostPhysicsPriority));
+	}
+
+	private void InsertSorted(List<IActionComponent> list, IActionComponent item, System.Comparison<IActionComponent> comparison)
+	{
+		int index = list.FindIndex(x => comparison(item, x) < 0);
+
+		if (index < 0)
+		{
+			list.Add(item);
+		}
+		else
+		{
+			list.Insert(index, item);
 		}
 	}
 
 	// Updates
 	public void PrePhysicsUpdate(float dt)
 	{
-		foreach (IActionComponent component in _components)
+		foreach (IActionComponent component in _prePhysics)
 		{
 			component.PrePhysicsUpdate(dt);
 		}
@@ -39,7 +84,7 @@ public partial class ActionOrchestrator : Node
 
 	public void PhysicsUpdate(float dt)
 	{
-		foreach (IActionComponent component in _components)
+		foreach (IActionComponent component in _physics)
 		{
 			component.PhysicsUpdate(dt);
 		}
@@ -47,7 +92,7 @@ public partial class ActionOrchestrator : Node
 
 	public void PostPhysicsUpdate(float dt)
 	{
-		foreach (IActionComponent component in _components)
+		foreach (IActionComponent component in _postPhysics)
 		{
 			component.PostPhysicsUpdate(dt);
 		}
